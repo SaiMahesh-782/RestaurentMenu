5646c3f89af65d002b5d889984d12db06885fd8c
Saimahesh-782 <saimahesh645@gmail.com>
2023-07-09 11:08:52 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Rest from './Components/Basics/Rest';
+
+function App(){
+  return (
+<>
+  <Rest/>;
+</>
+  );
+}
+
+
+
+
+export default App
+
+
---
+++ src/Components/Basics/MenuList.js
@@ -0,0 +1,126 @@
+const Menu = [
+    
+  {
+      id: 1,
+      image:"images/idli.jpg",
+      name: "idli",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/dosa.jpg",
+      name: "dosa",
+      category: "breakfast",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/uttapam.jpg",
+      name: "uttapam",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/vada.jpg",
+      name: "vada",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/poori.jpg",
+      name: "poori",
+      category: "breakfast",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/dhokla.jpg",
+      name: "dhokla",
+      category: "breakfast",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/veg thali.jpg",
+      name: "veg thali",
+      category: "dinner",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/roti.jpg",
+      name: "roti",
+      category: "dinner",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/rumali roti.jpg",
+      name: "rumali roti",
+      category: "dinner",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "../images/parota.jpg",
+      name: "parota",
+      category: "dinner",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    
+  
+    {
+      id: 11,
+      image: "../images/Mutton Biryani.jpg",
+      name: "chiken Biryani",
+      category: "lunch",
+      price: "160₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "../images/Fish Biryani.jpg",
+      name: "Fish Biryani",
+      category: "lunch",
+      price: "240₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 13,
+      image: "../images/Mutton Biryani.jpg",
+      name: "Mutton Biryani",
+      category: "lunch",
+      price: "200₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+   
+  ];
+  
+  export default Menu;
